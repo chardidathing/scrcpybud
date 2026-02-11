a734727e72fb7ad56cb03ef1596068f117a3a978
Charlie <charlie.mizutsuki@posteo.net>
2026-02-12 05:58:37 +1030

    bug: fork before qt starts, otherwise the child inherits a dead display connection :< (poor child)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <QApplication>
 #include <QCommandLineParser>
 #include <unistd.h>
+#include <cstring>
 #include "trayapp.h"
 
 int main(int argc, char *argv[]) {
+    // Fork before Qt connects to the display server
+    for (int i = 1; i < argc; ++i) {
+        if (std::strcmp(argv[i], "--detached") == 0) {
+            pid_t pid = fork();
+            if (pid < 0)
+                return 1;
+            if (pid > 0)
+                return 0;
+            setsid();
+            break;
+        }
+    }
+
     QApplication app(argc, argv);
     app.setApplicationName("scrcpybud");
     app.setQuitOnLastWindowClosed(false);
@@ -12,19 +26,6 @@ int main(int argc, char *argv[]) {
     parser.addHelpOption();
     parser.addOption({"detached", "Run in the background (fork to background)."});
     parser.process(app);
-
-    if (parser.isSet("detached")) {
-        pid_t pid = fork();
-        if (pid < 0) {
-            return 1;
-        }
-        if (pid > 0) {
-            // parent exits immediately
-            return 0;
-        }
-        // child continues as daemon? hopefully
-        setsid();
-    }
 
     TrayApp tray;
     return app.exec();
